c2bad90143023897a4716c889407918b5824298c
BudaSamir <Abdulrahman.Samir.Attia.Elseed.96@gmail.com>
2025-06-28 15:14:28 +0300

    Create Nodejs Project

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: 'config.env'});
+
+const server = express();
+
+
+server.get('/', (req, res) =>{
+    res.send("Abdulrahman samir attia");
+}  );
+
+
+
+const PORT = process.env.PORT || 8000;
+server.listen(
+    PORT, () =>{
+        console.log(`Server Running ON PORT ${PORT}.......`);
+    }
+);
